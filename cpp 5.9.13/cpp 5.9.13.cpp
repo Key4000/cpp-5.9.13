bfa1e1e3afa083a7759b7a1848c25b0c8da9c470
Key4000 <pvlzhizhin@mail.ru>
2022-08-14 12:37:03 +0500

    Добавьте файлы проекта.

---
+++ cpp 5.9.13/cpp 5.9.13.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Date {
+    short day;
+    short month;
+    short year;
+
+    Date(short day, short month, short year) {
+        this->day = day;
+        this->month = month;
+        this->year = year;
+    }
+
+    void input() {
+        cout << "Day:";
+        cin >> this->day;
+        cout << "Month:";
+        cin >> this->month;
+        cout << "Year:";
+        cin >> this->year;
+    }
+
+    void output() {
+        cout << "Day:" << this->day << " Month:" << this->month << " Year:" << this->year;
+    }
+};
+
+int main()
+{
+    Date one(17,03,1999);
+    one.input();
+    one.output();
+
+}
+
